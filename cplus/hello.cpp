718bdcfe5a333735e5b7d25acd4daf9fb3c31422
666 <111@123.com>
2025-05-14 19:20:22 +0800

    全部提交

---
+++ cplus/hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int max(int a, int b);
+int main()
+{
+    int x, y;
+    cout << "请输入x:";
+
+    cin >> x;
+    cout << "请输入y:";
+    cin >> y;
+    cout << "最大值是：" << max(x, y) << endl;
+    return 0;
+}
+int max(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
